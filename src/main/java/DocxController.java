4219aac929967b889f5031c9872514ae47faacf8
Vilkaz <admin@vilkas.de>
2016-03-08 17:28:21 +0100

    DTO wird durchiteriert und alle getter werden in die hashmap gesetzt

--- src/main/java/DocxController.java
+++ src/main/java/DocxController.java
@@ -1,11 +1,107 @@
+import org.docx4j.jaxb.Context;
+import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.ObjectFactory;
+
+import javax.xml.bind.JAXBException;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.HashMap;
 
 /**
  * Created by vkukanauskas on 08/03/2016.
  */
 public class DocxController {
 
-    public  static WordprocessingMLPackage getNewDocx(){
+    private static ObjectFactory factory = Context.getWmlObjectFactory();
+
+    public static WordprocessingMLPackage getNewDocx() {
         return new WordprocessingMLPackage();
+    }
+
+    public static WordprocessingMLPackage getDocxFromTemplate(String templateURL) {
+
+        WordprocessingMLPackage wordMLPackage = null;
+        try {
+            wordMLPackage = WordprocessingMLPackage
+                    .load(new File(templateURL));
+        } catch (Docx4JException e) {
+            e.printStackTrace();
+        }
+
+        return wordMLPackage;
+    }
+
+    public static void InsertDTOVariablesIntoDocx(WordprocessingMLPackage Docx, Object dto) {
+        MainDocumentPart mainDocumentPart = Docx.getMainDocumentPart();
+    }
+
+
+    /**
+     * It Works only, if all the Getters are for Strings
+     *
+     * @param dto
+     * @return
+     */
+    public static HashMap<String, String> getHashmapFromDTO(Object dto) {
+        HashMap<String, String> mappings = new HashMap<String, String>();
+        try {
+            for (PropertyDescriptor propertyDescriptor :
+                    Introspector.getBeanInfo(dto.getClass()).getPropertyDescriptors()) {
+                if (isGetter(propertyDescriptor)) {
+                    String value = getValueFromGetter(dto, propertyDescriptor);
+                    mappings.put(propertyDescriptor.getName(), value);
+                }
+
+
+                //mappings.put(propertyDescriptor.getName(),propertyDescriptor.getValue());
+                System.out.println("Public method found: " + propertyDescriptor.getReadMethod());
+                Method method = propertyDescriptor.getReadMethod();
+
+
+                Object def1;
+                try {
+                    try {
+                        def1 = method.invoke(dto, new Object[]{});
+                        System.out.println(def1.toString());
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+                String generic = method.toGenericString();
+                System.out.println("Value from that is : " + propertyDescriptor.getValue("getName"));
+            }
+        } catch (IntrospectionException e) {
+            e.printStackTrace();
+        }
+        return mappings;
+
+    }
+
+    private static boolean isGetter(PropertyDescriptor propertyDescriptor) {
+        String name = propertyDescriptor.getName();
+        return (!name.equals("Class") || !name.startsWith("get"));
+    }
+
+    private static String getValueFromGetter(Object dto, PropertyDescriptor propertyDescriptor) {
+        Method method = propertyDescriptor.getReadMethod();
+        String value = "";
+        try {
+            value = (String) method.invoke(dto, new Object[]{});
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        return value;
     }
 }
--- src/test/java/DocxControllerTest.java
+++ src/test/java/DocxControllerTest.java
@@ -2,23 +2,48 @@ import junit.framework.TestCase;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
 import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.URL;
+import java.util.HashMap;
 
 
 /**
  * Created by vkukanauskas on 08/03/2016.
  */
 public class DocxControllerTest extends TestCase {
+    final String rootDir = System.getProperty("user.dir");
+    final String templateDir  = rootDir+"/src/test/resources/";
+    final String templateUrl = templateDir+"test1.docx";
 
+    @Test
     public void testGetNewDocxClassName(){
         WordprocessingMLPackage newDocx = DocxController.getNewDocx();
         Assert.assertEquals("org.docx4j.openpackaging.packages.WordprocessingMLPackage",newDocx.getClass().getName().toString());
     }
 
+    @Test
     public void testGetNewDocxContentSize(){
         WordprocessingMLPackage newDocx = DocxController.getNewDocx();
         MainDocumentPart mainDocumentPart = newDocx.getMainDocumentPart();
        Assert.assertTrue(mainDocumentPart==null);
     }
+
+    @Test
+    public void testGetDocxFromTemplateByFile(){
+        WordprocessingMLPackage newDocx = DocxController.getDocxFromTemplate(templateUrl);
+        MainDocumentPart mainDocumentPart = newDocx.getMainDocumentPart();
+        Assert.assertTrue(mainDocumentPart!=null);
+    }
+
+    @Test
+    public void testGetHashmapFromDTO(){
+        CoverDTO dto = new CoverDTO("test Headline", "test name", "test_projekt");
+        HashMap<String, String> mapping =  DocxController.getHashmapFromDTO(dto);
+        System.out.println("test");
+    }
+
+
 
 
 }
